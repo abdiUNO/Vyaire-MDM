1391475afa0975bca9d418dce32970c888a2f10a
abdiUNO <amahamed@unomaha.edu>
2020-01-28 08:08:18 -0600

    added mockSearchResults func

--- src/appRedux/reducers/Customer.js
+++ src/appRedux/reducers/Customer.js
@@ -1,68 +1,66 @@
 import {
-	SEARCH_CUSTOMER,
+    SEARCH_CUSTOMER,
     SEARCH_CUSTOMER_SUCCESS,
     SHOW_MESSAGE,
     HIDE_MESSAGE,
     GET_CUSTOMER_DETAIL,
-    GET_CUSTOMER_DETAIL_SUCCESS
-} from "../../constants/ActionTypes";
+    GET_CUSTOMER_DETAIL_SUCCESS,
+} from '../../constants/ActionTypes';
 import Immutable from 'seamless-immutable';
 
 const INITIAL_STATE = {
     customerdata: [],
-    singleCustomerDetail:[],
+    singleCustomerDetail: [],
     fetching: false,
     error: null,
 };
 
 const customerReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) { 
-      case GET_CUSTOMER_DETAIL:{
-        return{
-          ...state,
-          fetching:true,
+    switch (action.type) {
+        case GET_CUSTOMER_DETAIL: {
+            return {
+                ...state,
+                fetching: true,
+            };
         }
-      }
-      case GET_CUSTOMER_DETAIL_SUCCESS:{
-        return{
-          ...state,
-          fetching:false,
-          singleCustomerDetail:action.payload
+        case GET_CUSTOMER_DETAIL_SUCCESS: {
+            return {
+                ...state,
+                fetching: false,
+                singleCustomerDetail: action.payload,
+            };
         }
-      }
-      case SEARCH_CUSTOMER:{
-        return{
-          ...state,
-          fetching:true,
+        case SEARCH_CUSTOMER: {
+            return {
+                ...state,
+                fetching: true,
+            };
         }
-      }    
-      case SEARCH_CUSTOMER_SUCCESS:{
-        return {
-          ...state,
-          fetching: false,
-          customerdata: action.payload
+        case SEARCH_CUSTOMER_SUCCESS: {
+            return {
+                ...state,
+                fetching: false,
+                customerdata: action.payload,
+            };
         }
-      }
-      
-    case SHOW_MESSAGE: {
-        return {
-          ...state,
-          alertMessage: action.payload,
-          showMessage: true,
-        }
-      }
-      case HIDE_MESSAGE: {
-        return {
-          ...state,
-          alertMessage: '',
-          showMessage: false,
-        }
-      }
-      default:
-      return state;
-  }
-}
 
-
+        case SHOW_MESSAGE: {
+            return {
+                ...state,
+                alertMessage: action.payload,
+                showMessage: true,
+            };
+        }
+        case HIDE_MESSAGE: {
+            return {
+                ...state,
+                alertMessage: '',
+                showMessage: false,
+            };
+        }
+        default:
+            return state;
+    }
+};
 
 export default customerReducer;
--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -1,8 +1,8 @@
-import {applyMiddleware, compose, createStore} from "redux";
-import reducers from "../reducers/index";
-import createHistory from "history/createBrowserHistory";
-import createSagaMiddleware from "redux-saga";
-import rootSaga from "../sagas/index";
+import { applyMiddleware, compose, createStore } from 'redux';
+import reducers from '../reducers/index';
+import createHistory from 'history/createBrowserHistory';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas/index';
 
 const history = createHistory();
 const sagaMiddleware = createSagaMiddleware();
@@ -11,10 +11,14 @@ const middlewares = [sagaMiddleware];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
-  const store = createStore(reducers, initialState,applyMiddleware(...middlewares));
+    const store = createStore(
+        reducers,
+        initialState,
+        applyMiddleware(...middlewares)
+    );
 
-  sagaMiddleware.run(rootSaga);
+    sagaMiddleware.run(rootSaga);
 
-  return store;
+    return store;
 }
-export {history};
+export { history };
